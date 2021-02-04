8fbb0d575c04aea80b7d144295998963747d4e6d
singh-ravi-siso <ravi16iiitg@gmail.com>
2021-02-04 13:12:09 +0530

    ...

---
+++ dist/precache-manifest.5830c6940221f20a6468fe1e40c6aba3.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ca5daa4f074f3952a5e0",
+    "url": "/css/app.2cc2b88b.css"
+  },
+  {
+    "revision": "dc6418d743dddc640199",
+    "url": "/css/chunk-vendors.1a3bff82.css"
+  },
+  {
+    "revision": "4dfed6ef341a1d1071cb862a4344536f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ca5daa4f074f3952a5e0",
+    "url": "/js/app.fd44b0cd.js"
+  },
+  {
+    "revision": "0e71614687b67dbf5080",
+    "url": "/js/chunk-2d22d5ba.becba828.js"
+  },
+  {
+    "revision": "dc6418d743dddc640199",
+    "url": "/js/chunk-vendors.655ea893.js"
+  },
+  {
+    "revision": "a3f9623de61da7e1fbed20ac3edda820",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
